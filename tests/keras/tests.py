85c55cd7d616d82f04da167ae330d602aa043649
Freddy Snijder <freddy@visionscapers.com>
2019-06-23 22:34:14 +0200

    Now keras and tf-keras have the same tests

--- tests/keras/tests.py
+++ tests/keras/tests.py
@@ -56,7 +56,7 @@ class TestIssuesKeras(unittest.TestCase):
         sess = tf.Session(config=TestIssuesKeras.TF_CONFIG)
         K.set_session(sess)
 
-    def test_single_gpu_float32_no_masking(self):
+    def test_single_gpu_float32_no_masking_calculate_dropout_noise_shape(self):
         dtype = self._enable_float32()
 
         batch_generator = FakeChatGenerator(
@@ -70,6 +70,69 @@ class TestIssuesKeras(unittest.TestCase):
                                                self.max_seq_length,
                                                self.num_unique_symbols,
                                                use_masking=False,
+                                               use_partially_known_dropout_noise_shape=False,
+                                               dtype=dtype)
+
+        with tf.device('/gpu:0'):
+            models = model_generator.stamp_train_model()
+
+            model = models["train_model"]
+
+        self._compile_model(model, lambda: Adam(lr=1e-4))
+
+        model.fit_generator(batch_generator,
+                            steps_per_epoch=len(batch_generator),
+                            epochs=5,
+                            verbose=1,
+                            max_queue_size=10,
+                            workers=3)
+
+    def test_single_gpu_float32_no_masking_use_partially_known_dropout_noise_shape(self):
+        dtype = self._enable_float32()
+
+        batch_generator = FakeChatGenerator(
+            num_unique_symbols=self.num_unique_symbols,
+            max_seq_length=self.max_seq_length,
+            batch_size=self.batch_size,
+            return_sample_weights=True,
+            dtype=dtype)
+
+        model_generator = Seq2SeqWithSubmodels(TestIssuesKeras.KERAS_CLASSES,
+                                               self.max_seq_length,
+                                               self.num_unique_symbols,
+                                               use_masking=False,
+                                               use_partially_known_dropout_noise_shape=True,
+                                               dtype=dtype)
+
+        with tf.device('/gpu:0'):
+            models = model_generator.stamp_train_model()
+
+            model = models["train_model"]
+
+        self._compile_model(model, lambda: Adam(lr=1e-4))
+
+        model.fit_generator(batch_generator,
+                            steps_per_epoch=len(batch_generator),
+                            epochs=5,
+                            verbose=1,
+                            max_queue_size=10,
+                            workers=3)
+
+    def test_single_gpu_float32_no_masking_no_dropout_noise_shape(self):
+        dtype = self._enable_float32()
+
+        batch_generator = FakeChatGenerator(
+            num_unique_symbols=self.num_unique_symbols,
+            max_seq_length=self.max_seq_length,
+            batch_size=self.batch_size,
+            return_sample_weights=True,
+            dtype=dtype)
+
+        model_generator = Seq2SeqWithSubmodels(TestIssuesKeras.KERAS_CLASSES,
+                                               self.max_seq_length,
+                                               self.num_unique_symbols,
+                                               use_masking=False,
+                                               disable_dropout_noise_shape=True,
                                                dtype=dtype)
 
         with tf.device('/gpu:0'):
